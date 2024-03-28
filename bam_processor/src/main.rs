ba1edf40f1a25ad8c4ada57a47e3d0806b7ebaaf
epiliper <epiliper@outlook.com>
2024-03-28 08:36:43 -0700

    simplifies read collection with BottomHashMap struct

--- bam_processor/src/main.rs
+++ bam_processor/src/main.rs
@@ -13,6 +13,26 @@ fn get_umi(record: &Record) -> String {
 // the data structure used to cluster UMIs. These are all inserted into a bottom_level dictionary.
 // top key: PositionKey
 // bottom key: ReadsCount
+struct BottomHashMap {
+    bottom_dict: PositionKey,
+}
+
+impl BottomHashMap {
+    fn update_dict(&mut self, position: &i32, key: i32, umi: &String, read: &Record) {
+        self.bottom_dict
+            .entry((*position).into())
+            .or_default()
+            .entry((key).into())
+            .or_default()
+            .entry(umi.into())
+            .or_insert(ReadsAndCount {
+                reads: Vec::new(),
+                count: 0,
+            })
+            .up(read)
+    }
+}
+
 type PositionKey = HashMap<i32, KeyUMI>; //every position has a key
 type KeyUMI = HashMap<i32, UMIReads>; // every key has a UMI
 type UMIReads = HashMap<String, ReadsAndCount>; // every UMI has a set of reads
@@ -20,78 +40,48 @@ type UMIReads = HashMap<String, ReadsAndCount>; // every UMI has a set of reads
 #[derive(Debug)]
 pub struct ReadsAndCount {
     pub reads: Vec<Record>,
-    pub count: i32
+    pub count: i32,
 }
 
 impl ReadsAndCount {
-    fn up(&mut self) {
+    fn up(&mut self, read: &Record) {
         self.count += 1;
+        self.reads.push(read.clone());
     }
 }
 
-fn update_dict(
-    position: &i32,
-    key: i32,
-    umi: &String,
-    read: &Record,
-    // read_count: i32,
-    final_dict: &mut PositionKey,
-) {
-    // at every position, at each key, for each umi, append a bam record to list of reads
-    final_dict
-        .entry((*position).into())
-        .or_default()
-        .entry((key).into())
-        .or_default()
-        .entry(umi.into())
-        .or_insert(ReadsAndCount {
-            reads: Vec::new(),
-            count: 0
-        })
-            .reads.push(read.clone());
-
-    // for every record appended, increment the count field by 1
-    final_dict
-        .entry((*position).into())
-        .or_default()
-        .entry((key).into())
-        .or_default()
-        .entry(umi.into())
-        .or_insert(ReadsAndCount {
-            reads: Vec::new(),
-            count: 0
-        })
-            .up();
-}
-
 fn main() {
-    let mut bottom_dict: PositionKey = HashMap::new();
+    // let mut bottom_dict: PositionKey = HashMap::new();
+    let mut master_dic = BottomHashMap {
+        bottom_dict: HashMap::new(),
+    };
 
     let args: Vec<String> = env::args().collect();
     let input_file = &args[1];
     let bam = bam::BamReader::from_path(&input_file, 4).unwrap();
     let mut n: i64 = 0;
 
+    // implement get_bundles()
     for read in bam {
         if read.as_ref().unwrap().flag().is_reverse_strand()
+            | !read.as_ref().unwrap().flag().is_mapped()
         // if read.unwrap().flag().is_reverse_strand()
         {
             continue;
         } else {
             let r1 = read.as_ref().unwrap();
 
-            update_dict(&r1.start(), 0, &get_umi(&r1), &r1, &mut bottom_dict);
+            master_dic.update_dict(&r1.start(), 0, &get_umi(&r1), &r1);
             n += 1;
-            
+
             //print the millionth iteration dict for debugging
             if n == 1_000_000 {
-                println!("{:?}", &bottom_dict);
+                println!("{:?}", &master_dic.bottom_dict);
             }
 
             if n % 100_000 == 0 {
-                println!{"Processed {n} reads" }
-            } 
+                println! {"Processed {n} reads" }
+            }
         }
-
     }
 }
