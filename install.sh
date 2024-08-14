80ad125ca28cf6d1049613f13e9693bc48a48d43
epiliper <epiliper@outlook.com>
2024-08-14 11:00:02 -0700

    simplifies installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd bam_processor
-cargo build --release
+cargo build --release 
 
 cd ../multibam 
 cargo build --release
@@ -15,13 +15,17 @@ python3.12 -m pip install -r requirements.txt
 deactivate
 
 home_dir=$PWD
+install_dir="$HOME/.cargo/bin"
 
-echo "#!/usr/bin/env bash" > $HOME/.cargo/bin/rumina
-echo "source $home_dir/python_env/bin/activate" >> $HOME/.cargo/bin/rumina
-echo "python3.12 $home_dir/main.py \"\$@\"" >> $HOME/.cargo/bin/rumina
-echo "deactivate" >> $HOME/.cargo/bin/rumina
-chmod +x $HOME/.cargo/bin/rumina
+if [ ! -d "$install_dir" ]; then
+	mkdir -p "$install_dir"
+fi
 
-
-
-
+# echo "#!/usr/bin/env bash" > "$install_dir"/rumina
+echo "#!/usr/bin/env bash" > "$install_dir"/rumina
+echo "source $home_dir/python_env/bin/activate" >> "$install_dir"/rumina
+echo "python3.12 $home_dir/main.py \"\$@\"" >> "$install_dir"/rumina
+echo "deactivate" >> "$install_dir"/rumina
+chmod +x "$install_dir"/rumina
+echo "-------------------------"
+echo "RUMINA installed successfully!\nTry running "rumina -h" to see the help screen.\nRUMINA installation dir: $home_dir"
