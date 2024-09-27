fc5c355ff171539e9cc14dcbb0729bca6c568747
epiliper <epiliper@outlook.com>
2024-09-27 13:04:17 -0700

    multibam rewrite: split/nonsplit consistent count
    tested with shotgun data: 100bp split window and non split yield
    identical read count with pair merging

--- multibam/src/main.rs
+++ multibam/src/main.rs
@@ -1,9 +1,6 @@
-use bam::bam_writer::BamWriterBuilder;
-use bam::{Record, RecordWriter};
-use indexmap::IndexMap;
-use rayon::prelude::*;
+use rust_htslib::bam::ext::BamRecordExtensions;
+use rust_htslib::bam::{Format, Header, IndexedReader, Read, Writer};
 use std::fs;
-use std::ops::Range;
 use std::path::Path;
 
 use clap::Parser;
@@ -25,87 +22,86 @@ struct Args {
 }
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    //
     let args = Args::parse();
 
     let input = Path::new(&args.input);
-    let add_threads = (args.threads - 1) as u16;
+    let out = Path::new(&args.out);
 
     let output_dir = Path::parent(&input).unwrap().join(&args.out);
     if !output_dir.exists() {
-        fs::create_dir(&output_dir);
+        let _ = fs::create_dir(&output_dir);
     }
 
     let output_dir = fs::canonicalize(&output_dir).unwrap();
 
-    let mut input_bam = bam::BamReader::from_path(&input, add_threads).unwrap();
-    let input_header = bam::BamReader::from_path(&input, 0)
-        .unwrap()
-        .header()
-        .clone();
-    let max_pos = input_header.reference_len(0).unwrap() as i32;
-    let mut pos_range = (0..max_pos).collect::<Vec<i32>>();
+    let mut bam_reader = IndexedReader::from_path(&input).unwrap();
+    bam_reader.set_threads(args.threads).unwrap();
+    let header = Header::from_template(bam_reader.header());
 
-    // let chunk_size = &args[3].parse::<usize>().unwrap();
-    let chunk_size = args.split_window;
+    let ref_count = bam_reader.header().target_count();
+    let window_size = args.split_window as i64;
 
-    let mut ranges_of_reads: IndexMap<Range<i32>, Vec<Record>> = IndexMap::new();
-    let mut ranges: Vec<Range<i32>> = Vec::new();
+    let mut write_counter = 0;
 
-    for idx_array in pos_range.chunks_mut(chunk_size) {
-        let read_range = Range {
-            start: *idx_array.iter().min().unwrap(),
-            end: *idx_array.iter().max().unwrap(),
-        };
+    // for every reference, split it into windows by split_window
+    for tid in 0..ref_count {
+        println!("{tid}");
+        let max_pos = bam_reader.header().target_len(tid).unwrap() as i64;
 
-        print! {"\r{:?}", read_range};
+        let mut ranges: Vec<[i64; 2]> = Vec::new();
 
-        ranges_of_reads
-            .entry(read_range.clone())
-            .or_insert(Vec::new());
-        ranges.push(read_range);
-    }
+        let mut j = 0;
+        let mut i = 0;
 
-    for read in input_bam {
-        let mut pos = i32::MAX;
-        let r = read.unwrap();
-        if r.flag().is_reverse_strand() {
-            pos = r.calculate_end();
-        } else {
-            pos = r.start();
+        while i <= max_pos - window_size {
+            j = i;
+            i = j + window_size;
+
+            ranges.push([j, i]);
         }
 
-        for range in &ranges {
-            match range.contains(&pos) {
-                true => {
-                    ranges_of_reads[range].push(r);
-                    break;
+        // add the remainder
+        // fetch end value is exclusive, so adding 1
+        ranges.push([i, max_pos + 1]);
+
+        // write to a subfile for each window
+        for (idx, range) in ranges.drain(0..).enumerate() {
+            println!("{:?}", range);
+            let mut reads_to_write_f = Vec::with_capacity(1_000_000);
+
+            let start = range[0];
+            let end = range[1];
+
+            let _ = bam_reader.fetch((tid, start, end + 300));
+
+            for r in bam_reader.records() {
+                let read = r.unwrap();
+
+                if read.is_reverse() {
+                    if read.reference_end() < end && read.reference_end() >= start {
+                        reads_to_write_f.push(read);
+                    }
+                } else if read.reference_start() < end && read.reference_start() >= start {
+                    reads_to_write_f.push(read);
                 }
-                false => {}
-            };
-        }
-    }
+            }
 
-    ranges_of_reads
-        .par_values_mut()
-        .enumerate()
-        .for_each(|(idx, read_bundle)| {
             let infile = Path::file_name(input).unwrap().to_str().unwrap();
             let prefix = infile.split(".bam").next().unwrap();
-            let outfile = format!("{}{}{}{}", prefix, ".", idx, ".bam");
 
-            print! {"\rWriting {:?} reads to {:?}", read_bundle.len(), outfile};
+            let outfile_f = format!("{}{}{}{}{}{}", prefix, ".", "f", tid, idx, ".bam");
 
-            let mut output_writer = BamWriterBuilder::from_path(
-                &mut BamWriterBuilder::new().additional_threads(add_threads),
-                &output_dir.join(outfile),
-                input_header.clone(),
-            )
-            .unwrap();
+            let mut bam_writer_f =
+                Writer::from_path(&output_dir.join(&outfile_f), &header, Format::Bam).unwrap();
 
-            read_bundle
-                .drain(0..)
-                .for_each(|x| output_writer.write(&x).unwrap());
-        });
+            bam_writer_f.set_threads(args.threads).unwrap();
+
+            reads_to_write_f.iter().for_each(|read| {
+                bam_writer_f.write(read).unwrap();
+                write_counter += 1;
+            });
+        }
+    }
+
+    println!("Written {} reads...", write_counter);
 }
