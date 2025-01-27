edc816b4cda81065a316016a928a290a11299228
epiliper <epiliper@outlook.com>
2025-01-27 08:09:12 -0800

    refactor: makes cov_reporter standalone script

--- cov_reporter.py
+++ cov_reporter.py
@@ -1,13 +1,17 @@
+#! /usr/bin/env python3
+
 import pandas as pd
 import os
 import warnings
 import pysam
 from concurrent.futures import ThreadPoolExecutor
-from args import init_args
+import argparse
+
+parser = argparse.ArgumentParser(description="A simple python wrapper over samtools coverage, with multithreading for parallel processing of references.")
+parser.add_argument("infile", help = "bam file to get coverage reporting for")
+parser.add_argument("--threads", default = os.cpu_count(), help = "number of threads to use", required = False)
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
-args = init_args()
-
 
 def get_coverage(input_file, region):
     region_df = pd.DataFrame()
@@ -22,7 +26,7 @@ def get_coverage(input_file, region):
     return region_df
 
 
-def generate_cov_depth_report(original_file, final_file):
+def generate_cov_depth_report(final_file):
     sort_and_index(final_file)
     cov_report = pd.DataFrame()
     with pysam.AlignmentFile(final_file, "rb") as outbam:
@@ -39,3 +43,19 @@ def generate_cov_depth_report(original_file, final_file):
     save_dir = os.path.dirname(final_file)
     cov_tsv = os.path.join(save_dir, final_file.split(".bam")[0] + "_coverage.tsv")
     cov_report.to_csv(cov_tsv, sep="\t", index=None)
+
+def sort_and_index(output_file):
+    print("sorting and indexing output BAM...\r")
+
+    temp_file = output_file.split(".bam")[0] + "_s.bam"
+    os.rename(output_file, temp_file)
+
+    pysam.sort(f"-@ {args.threads}", temp_file, "-o", output_file)
+    os.remove(temp_file)
+
+    pysam.index(f"-@ {args.threads}", output_file)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    generate_cov_depth_report(args.infile)
+
